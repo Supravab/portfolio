b3b66b49701fe13326acbc0af2bdaaf5a610893a
Supravab <ranjitapokharel16@gmail.com>
2025-05-07 19:36:23 +0545

    blog page

--- script.js
+++ script.js
@@ -215,3 +215,62 @@ function contactPage() {
   output.appendChild(contactMe);
  setTimeout(() =>window.location.href="contact.html", 530);
 } ;
+
+//blog part
+const parent = document.querySelector(".container-fluid");
+parent.addEventListener("click", function (e) {
+  if (e.target && e.target.classList.contains("collapse-content")) {
+    const post = e.target.closest(".content");
+  }
+});
+const continueReading = document.getElementById("continue-button");
+const readLess = document.querySelectorAll(".collapse-content");
+const contentArea = document.getElementById("blog-content-area");
+contentArea.classList.add("hide-blog");
+const featuredPosts = document.querySelectorAll('[id^="featured-post-"]');
+const recentPosts = document.querySelectorAll('[id^="recent-post-"]');
+const heroTitle = document.querySelector(".display-4");
+const heroDescription = document.querySelector(".hero-description");
+function updateHero(title, description, content) {
+  heroTitle.innerHTML = title.textContent;
+  heroDescription.innerHTML = description.textContent;
+  contentArea.innerHTML = "";
+  content.forEach((node) => {
+    contentArea.appendChild(node.cloneNode(true));
+  });
+  continueReading.classList.add("hide-blog");
+  contentArea.classList.remove("hide-blog");
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+}
+featuredPosts.forEach((featuredPosts) =>
+  featuredPosts.addEventListener("click", function () {
+    let title = featuredPosts.querySelector("h3");
+    let description = featuredPosts.querySelector("p");
+    let content = featuredPosts.querySelector(".content");
+    let clonedContent = content.cloneNode(true).childNodes;
+    updateHero(title, description, clonedContent);
+  })
+);
+recentPosts.forEach((recentPosts) =>
+  recentPosts.addEventListener("click", function () {
+    let title = recentPosts.querySelector("h6");
+    let description = recentPosts.querySelector(".description");
+    let content = recentPosts.querySelector(".content");
+    let clonedContent = content.cloneNode(true).childNodes;
+    updateHero(title, description, clonedContent);
+  })
+);
+continueReading.addEventListener("click", function () {
+  contentArea.classList.remove("hide-blog");
+  continueReading.classList.add("hide-blog");
+});
+readLess.forEach((readLess) => {
+  readLess.addEventListener("click", function () {
+    contentArea.classList.add("hide-blog");
+    continueReading.classList.remove("hide-blog");
+  });
+});
