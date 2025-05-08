a3fc0c8e38e501582ef107967f1b1bfe97ba8b7d
Supravab <ranjitapokharel16@gmail.com>
2025-05-08 15:19:39 +0545

    blog project completed

--- blog.js
+++ blog.js
@@ -4,35 +4,51 @@ contentArea.classList.add("hide-blog");
 function updateHero(title, description, content) {
   heroTitle.innerHTML = title.textContent;
   heroDescription.innerHTML = description.textContent;
+  if (content) {
+    contentArea.innerHTML = "";
+    content.forEach((node) => {
+      contentArea.appendChild(node.cloneNode(true));
+    });
+    continueReading.classList.add("hide-blog");
+    contentArea.classList.remove("hide-blog");
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  }
+}
+function recentPost(x) {
+  let title = x.querySelector("h6");
+  let description = x.querySelector("small");
+  updateHero(title, description);
   contentArea.innerHTML = "";
-  content.forEach((node) => {
-    contentArea.appendChild(node.cloneNode(true));
-  });
-  continueReading.classList.add("hide-blog");
-  contentArea.classList.remove("hide-blog");
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: "smooth",
-  });
+  let closeBtn = document.createElement("button");
+  closeBtn.classList.add("btn", "close-btn", "collapse-content");
+  closeBtn.innerHTML = "Close The Blog...";
+  contentArea.appendChild(closeBtn);
 }
-function recentPost1(x) {
-  let title = x.querySelector("h6");
-  let description = x.querySelector("small");
-  updateHero(title, description);
+
+function updateContent(content) {
+  let copiedContent = content.cloneNode(true);
+  copiedContent.classList.remove("d-none");
+  contentArea.appendChild(copiedContent);
+  window.scrollTo(0,0);
 }
-function recentPost2(x) {
-  let title = x.querySelector("h6");
-  let description = x.querySelector("small");
-  let content = "";
-  updateHero(title, description);
+function Post1() {
+  let content = document.querySelector(".recent-content-1");
+  updateContent(content);
 }
-function recentPost3(x) {
-  let title = x.querySelector("h6");
-  let description = x.querySelector("small");
-  let content = "";
-  updateHero(title, description);
+
+function Post2() {
+  let content = document.querySelector(".recent-content-2");
+  updateContent(content);
 }
+function Post3() {
+  let content = document.querySelector(".recent-content-3");
+  updateContent(content);
+}
+
 const parent = document.querySelector(".container-fluid");
 const continueReading = document.getElementById("continue-button");
 parent.addEventListener("click", function (e) {
@@ -42,13 +58,17 @@ parent.addEventListener("click", function (e) {
     contentCard.classList.add("hide-blog");
     continueReading.classList.remove("hide-blog");
   }
-  if (e.target && e.target.classList.contains("recent-post")) {
-    if (e.target.id.includes("recent-post-1")) {
-      recentPost1(e.target);
-    } else if (e.target.id.includes("recent-post-2")) {
-      recentPost2(e.target);
-    } else if (e.target.id.includes("recent-post-3")) {
-      recentPost3(e.target);
+  const thisIsIt = e.target.closest(".recent-post");
+  if (thisIsIt) {
+    if (thisIsIt.id.includes("recent-post-1")) {
+      recentPost(thisIsIt);
+      Post1();
+    } else if (thisIsIt.id.includes("recent-post-2")) {
+      recentPost(thisIsIt);
+      Post2();
+    } else if (thisIsIt.id.includes("recent-post-3")) {
+      recentPost(thisIsIt);
+      Post3();
     }
   }
 });
